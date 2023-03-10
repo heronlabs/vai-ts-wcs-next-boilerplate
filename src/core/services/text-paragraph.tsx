19745718163fe5619c3341a8e0f98827926853a4
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-03-10 14:02:29 -0300

    #deploy

--- src/core/services/text-paragraph.tsx
+++ src/core/services/text-paragraph.tsx
@@ -1,11 +1,13 @@
+import {v4} from 'uuid';
+
 export const applyBreakline = (text: string | undefined) => {
   return (
     <>
       {text?.split(/\n/g).map(line => (
-        <>
+        <span key={v4()}>
           {line}
           <br />
-        </>
+        </span>
       ))}
     </>
   );
